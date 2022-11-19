eed419305942cca6e246cec2a378f0bd2b5018b0
Losev Andrei <losev@aheadworks.com>
2022-11-20 00:12:25 +0300

    q

--- lua/settings.lua
+++ lua/settings.lua
@@ -16,7 +16,6 @@ lvim.leader = "space"
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 lvim.builtin.alpha.active = true
 lvim.builtin.alpha.mode = "dashboard"
-lvim.builtin.notify.active = true
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
